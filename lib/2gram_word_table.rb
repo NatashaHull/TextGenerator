dfb2bc0df50f5f7633b84bd56595955f405590bd
Natasha Hull <musicalnat@gmail.com>
2013-12-27 16:06:59 -0800

    Added back in 2gram word table

---
+++ lib/2gram_word_table.rb
@@ -0,0 +1,50 @@
+class WordTable
+  def initialize(filename=nil)
+    @table = Hash.new
+    add_file_to_table(filename) if !!filename
+  end
+
+  def add_file_to_table(filename)
+    doc = parse_file(filename)
+    @table["."] ||= []
+    @table["."] << doc[0]
+    doc.each_index do |word_i|
+      break if word_i == (doc.length-1)
+      @table[doc[word_i]] ||= []
+      @table[doc[word_i]] << doc[word_i+1]
+    end
+  end
+
+  def generate_text
+    text = @table["."].sample
+    text += text_loop(text)
+    puts text.gsub(/\s+([.,!?])/, '\1')
+  end
+
+  private
+
+    def parse_file(filename)
+      File.readlines(filename).join.gsub(/([.,!?])/, ' \1').split(/\s+/)
+    end
+
+    def text_loop(curr_word)
+      next_word = @table[curr_word].sample
+      text = " #{next_word}"
+      if [".", "?", "!"].include?(next_word) && should_stop?
+        text
+      else
+        text + text_loop(next_word)
+      end
+    end
+
+    def should_stop?
+      rand < 0.6
+    end
+end
+
+#Tests
+puts Time.now
+table = WordTable.new("philosophical_works/kant.txt")
+table.add_file_to_table("philosophical_works/hume.txt")
+table.generate_text
+puts Time.now
--- text_generator_app.rb
+++ text_generator_app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'redis'
-require_relative 'lib/word_table.rb'
+require_relative 'lib/3gram_word_table.rb'
 
 def create_word_table
   table = WordTable.new("philosophical_works/hume.txt")
