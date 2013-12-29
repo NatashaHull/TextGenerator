2d05e2e1e6c5e04a0f3b4292373baaaf65467ac1
Natasha Hull <musicalnat@gmail.com>
2013-12-29 13:36:48 -0800

    Still working on Redis connection refused bug

--- app.rb
+++ app.rb
@@ -17,10 +17,9 @@ if ENV["REDISTOGO_URL"]
   uri = URI.parse(ENV["REDISTOGO_URL"])
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 # elsif Sinatra::Base.development?
-  # REDIS = Redis.new
+  # REDIS =r Redis.new
 else
-  uri = URI.parse("markovosophers.tc6xga.0001.usw1.cache.amazonaws.com:6379")
-  REDIS = Redis.new(:host => uri.host, :port => uri.port)
+  REDIS = Redis.new(:host => "markovosophers.tc6xga.0001.usw1.cache.amazonaws.com", :port => 6379)
 end
 
 table = create_word_table
