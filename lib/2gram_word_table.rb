2662b13f795a6b1a7af5f41e479cc5e0a5817ac6
Natasha Hull <musicalnat@gmail.com>
2014-01-02 15:37:13 -0800

    Removed merge! method and protected table method from 2gram word table

--- lib/2gram_word_table.rb
+++ lib/2gram_word_table.rb
@@ -21,16 +21,6 @@ class WordTable
     puts text.gsub(/\s+([.,!?])/, '\1')
   end
 
-  def merge!(other_table)
-    @table.merge!(other_table.table) do |key, old_val, new_val|
-      old_val + new_val
-    end
-  end
-
-  protected
-
-    attr_reader :table
-
   private
 
     def parse_file(filename)
